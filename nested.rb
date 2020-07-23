00d9ed61398fa98a92314a21c321a4a274b5bf08
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-23 14:52:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -138,7 +138,7 @@ def adding_to_dennis
       }
     }
     
-    programmer_hash[:dennis_ritchie][:languages].add("Assembly")
+    programmer_hash[:dennis_ritchie][:languages].a("Assembly")
     programmer_hash
 
 end
